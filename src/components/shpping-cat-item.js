aa57b10951854c1c96037d51164ccf7969f3a97c
king-ran <924007244@qq.com>
2018-03-06 17:31:00 +0800

    shopping

--- src/components/shpping-cat-item.js
+++ src/components/shpping-cat-item.js
@@ -4,19 +4,34 @@ import { render } from 'react-dom';
 export default class ShoppingCatItems extends Component {
   constructor(props) {
     super(props);
+    this.state = { data: props.data };
+
     this.handlepProductNumChange = this.handlepProductNumChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChcked = this.handleChcked.bind(this);
   }
   handlepProductNumChange(id, status) {
-    // this.props.numberChang(id, status);
-    console.log(id, status);
+    if (!status && this.state.data.number === 1) return
+    this.props.numberChang(id, status);
+    this.setState({ data: this.props.data });
+  }
+  handleChange(event, id) {
+    console.log(11111111);
+    console.log(event.target.value);
+    const value = event.target.value;
+    this.props.numberChang(id, false, value);
+    this.setState({ data: this.props.data });
+  }
+  handleChcked(id) {
+    this.props.chckedStatus(id);
   }
   render() {
     return (
       <div className='' style={{ height: '4rem', width: '100%', display: 'flex', padding: '.2rem 0 .2rem 0', backgroundColor: '#fff' }}>
-        <div style={{ width: '2rem', height: '4rem', textAlign: 'center', lineHeight: '4rem' }}>
+        <div onClick={() => { this.handleChcked(this.props.data.id) }} style={{ width: '2rem', height: '4rem', textAlign: 'center', lineHeight: '4rem' }}>
           {this.props.data.status ?
-            <span className='font' style={{ color: "red", fontSize: '.8rem' }}>&#xe90d;</span> :
-            <span className='font' style={{ fontSize: '.8rem' }}>&#xe90d;</span>
+            (<span className='font' style={{ color: "red", fontSize: '.8rem' }}>&#xe90d;</span>) :
+            (<span className='font' style={{ fontSize: '.8rem' }}>&#xe90d;</span>)
           }
         </div>
         <div style={{ display: 'flex' }}>
@@ -35,20 +50,23 @@ export default class ShoppingCatItems extends Component {
               <div style={{ fontSize: '.7rem', height: '1.5rem', lineHeight: '1.5erm', color: 'red' }}>现金:{this.props.data.price}</div>
               <div style={{ height: '1rem', width: '3.5rem', display: 'flex', border: '1px solid #ebebeb', margin: 'auto 0' }}>
                 <div style={{ heighe: '1rem', width: '1rem', border: '1px solid #ebebeb', lineHeight: '1rem', textAlign: 'center' }}>
-                  <span onClick={this.handlepProductNumChange(this.props.data.number, false)} style={{ display: 'block', marginTop: "-0.1rem" }}>-</span>
+                  {this.state.data.number === 1 ?
+                    (<span style={{ display: 'block', marginTop: "-0.1rem" }} className='fontColorGray'>-</span>)
+                    : (<span onClick={() => { this.handlepProductNumChange(this.props.data.id, false) }} style={{ display: 'block', marginTop: "-0.1rem" }}>-</span>)}
+                  {/* <span onClick={() => { this.handlepProductNumChange(this.props.data.id, false) }} style={{ display: 'block', marginTop: "-0.1rem" }}>-</span> */}
                 </div>
                 {/* <div  style={{ heighe: '1rem', width: '1.5rem',border: '1px solid #ebebeb', lineHeight: '1rem',fontSize:'.75rem', textAlign: 'center'  }}> */}
 
-                <input type='number' value={this.props.data.number} style={{ heighe: '1rem', width: '1.5rem', border: '1px solid #ebebeb', display: 'block', textAlign: 'center' }} />
+                <input type='number' value={this.state.data.number} onChange={e => this.handleChange(e, this.props.data.id)} style={{ heighe: '1rem', width: '1.5rem', border: '1px solid #ebebeb', display: 'block', textAlign: 'center' }} />
                 {/* </div> */}
                 <div style={{ heighe: '1rem', width: '1rem', border: '1px solid #ebebeb', lineHeight: '1rem', textAlign: 'center' }}>
-                  <span onClick={this.handlepProductNumChange(this.props.data.number, true)} style={{ display: 'block', marginTop: "-0.1rem" }}>+</span>
+                  <span onClick={() => { this.handlepProductNumChange(this.props.data.id, true) }} style={{ display: 'block', marginTop: "-0.1rem" }}>+</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div >
+      </div>
     )
   }
 }
--- src/page/shoppoingCat.js
+++ src/page/shoppoingCat.js
@@ -5,11 +5,77 @@ import HeaderReact from '../components/header-react';
 import ShoppingCatItems from '../components/shpping-cat-item';
 
 export default class ShoppingCat extends Component {
+  constructor(props) {
+    super(props);
+    this.numChange = this.numChange.bind(this);
+    this.SwitchAllStatus = this.SwitchAllStatus.bind(this);
+    this.switchStatus = this.switchStatus.bind(this);
+    this.isRed = this.isRed.bind(this);
+    this.state = { isAll: false }
+    this.data = [
+      { status: true, id: '1', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '2', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '3', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '4', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '5', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '6', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '7', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '8', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },
+      { status: true, id: '9', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: 1, price: '60' },]
+  }
+  componentDidMount() {
+
+    this.setState({ isAll: this.isRed() });
+    console.log(this.state.isAll);
+  }
+  numChange(id, isAdd, number) {
+
+    if (number !== undefined) {
+      number = number === '' ? 1 : number;
+      for (let i = 0, length = this.data.length; i < length; i++) {
+        if (this.data[i].id === id) {
+
+          return this.data[i].number = number;
+        }
+      }
+    }
+    else {
+      for (let i = 0, length = this.data.length; i < length; i++) {
+        if (this.data[i].id === id) {
+          return isAdd ? ++this.data[i].number : --this.data[i].number;
+        }
+      }
+    }
+  }
+  SwitchAllStatus(status) {
+    this.setState({ isAll: !status })
+    this.data = this.data.map(v => {
+      v.status = !status;
+      return v
+    })
+
+    console.log(this.data);
+    console.log(status);
+  }
+  switchStatus(id) {
+    console.log(id);
+    for (let i = 0, length = this.data.length; i < length; i++) {
+      if (this.data[i].id === id) {
+        this.data[i].status = !this.data[i].status;
+        // this.isRed();
+        this.setState({ isAll: this.isRed() })
+        console.log(this.data[i]);
+      }
+    }
+  }
+  isRed() {
+    for (let i of this.data) {
+      if (!i.status) return false;
+    }
+    return true;
+  }
   render() {
-    const catData = [
-      { statu: true, id: '1', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: '1', price: '60' },
-      { statu: true, id: '2', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: '1', price: '60' },
-      { statu: true, id: '3', name: '王老吉荣誉产品顶级阿胶红枣饮料310ml*12瓶/箱', imgUrl: '', number: '1', price: '60' },]
+    const catData = this.data;
     return (
       <div style={{ height: '100%', width: '100%', backgroundColor: '#fff' }}>
         {/* <HeaderReact /> */}
@@ -17,21 +83,20 @@ export default class ShoppingCat extends Component {
         <div style={{ height: '50px', width: '100%' }}></div>
         <div className='catContent ' style={{ height: '100%', width: '100%', paddingTop: '10px', backgroundColor: '#f3f3f3' }}  >
           <div className='catContentTop defaultBackgroud' style={{ height: '1.5rem', width: '100%', lineHeight: '1.5rem', fontSize: '.8rem', display: 'flex' }}>
-            <div className='contentTopIcon' style={{ height: '1.5rem', width: '2rem', textAlign: 'center', marginTop: 'auto', marginBottom: 'auto' }} >
-              <span className='font' style={{ fontSize: '.8rem' }} >&#xe90d;</span>
+            <div onClick={() => { this.SwitchAllStatus(this.state.isAll) }} className='contentTopIcon' style={{ height: '1.5rem', width: '2rem', textAlign: 'center', marginTop: 'auto', marginBottom: 'auto' }} >
+              <span className='font' style={this.state.isAll ? { fontSize: '.8rem', color: "red" } : { fontSize: '.8rem' }} >&#xe90d;</span>
             </div>
-            <div className='contentTopText'>
-
+            <div className='contentTopText' >
               <span className='font' style={{ fontSize: '.8rem', color: 'red', paddingRight: '5px' }} >&#xe90c;</span>
               紫层商城
             </div>
 
           </div>
-          <div className='catContentItem' style={{ height: '100%', width: '100%' }}>
-              {catData.map(item => (
-                  <ShoppingCatItems key={item.id} data={item} />
-              ))}
-            </div>
+          <div className='catContentItem' style={{ height: '100%', width: '100%',paddingBottom:'60px' }}>
+            {catData.map(item => (
+              <ShoppingCatItems numberChang={this.numChange} chckedStatus={this.switchStatus} key={item.id} data={item} />
+            ))}
+          </div>
         </div>
       </div>
     )
